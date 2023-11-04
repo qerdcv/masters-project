13829e7f35ca2974c11f0417beecfed33d86fc32
qerdcv <vadym.tishchenko2@gmail.com>
2023-11-04 14:43:25 +0200

    Investigate cache

--- lti/app/app.py
+++ lti/app/app.py
@@ -16,15 +16,17 @@ from pylti1p3.lineitem import LineItem
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
 
-
 app = Flask('lti-imim-22-1', template_folder='templates', static_folder='static')
 sock = Sock(app)
 
-config = {
+CACHE_DIR = mkdtemp()
+print(CACHE_DIR)
+
+app.config.from_mapping({
     "DEBUG": True,
     "ENV": "development",
     "CACHE_TYPE": "FileSystemCache",
-    "CACHE_DIR": mkdtemp(),
+    "CACHE_DIR": CACHE_DIR,
     "CACHE_DEFAULT_TIMEOUT": 600,
     "SECRET_KEY": "replace-me",
     "SESSION_TYPE": "filesystem",
@@ -37,28 +39,11 @@ config = {
     "SOCK_SERVER_OPTIONS": {
         'ping_interval': 25
     }
-}
+})
 
-app.config.from_mapping(config)
 cache = Cache(app)
 
 PAGE_TITLE = 'LTI'
-
-
-class ExtendedFlaskMessageLaunch(FlaskMessageLaunch):
-
-    def validate_nonce(self):
-        """
-        Probably it is bug on "https://lti-ri.imsglobal.org":
-        site passes invalid "nonce" value during deep links launch.
-        Because of this in case of iss == http://imsglobal.org just skip nonce validation.
-
-        """
-        iss = self.get_iss()
-        deep_link_launch = self.is_deep_link_launch()
-        if iss == "http://imsglobal.org" and deep_link_launch:
-            return self
-        return super().validate_nonce()
 
 
 def get_lti_config_path():
@@ -95,8 +80,8 @@ def login():
         raise Exception('Missing "target_link_uri" param')
 
     oidc_login = FlaskOIDCLogin(flask_request, tool_conf, launch_data_storage=launch_data_storage)
-    return oidc_login\
-        .enable_check_cookies()\
+    return oidc_login \
+        .enable_check_cookies() \
         .redirect(target_link_uri)
 
 
@@ -105,7 +90,7 @@ def launch():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     flask_request = FlaskRequest()
     launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch(flask_request, tool_conf, launch_data_storage=launch_data_storage)
+    message_launch = FlaskMessageLaunch(flask_request, tool_conf, launch_data_storage=launch_data_storage)
     message_launch_data = message_launch.get_launch_data()
 
     tpl_kwargs = {
@@ -140,8 +125,8 @@ def score(launch_id, earned_score):
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     flask_request = FlaskRequest()
     launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
-                                                           launch_data_storage=launch_data_storage)
+    message_launch = FlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
+                                                   launch_data_storage=launch_data_storage)
 
     resource_link_id = message_launch.get_launch_data() \
         .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
@@ -179,8 +164,8 @@ def scoreboard(launch_id):
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     flask_request = FlaskRequest()
     launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
-                                                           launch_data_storage=launch_data_storage)
+    message_launch = FlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
+                                                   launch_data_storage=launch_data_storage)
 
     resource_link_id = message_launch.get_launch_data() \
         .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
@@ -291,7 +276,7 @@ def client_sock(ws: Server, email: str):
     clients[email] = ws
 
     if email in servers:
-        send_client(email, {'event': 'connected', 'message': ''})
+        send_client(email, {'event': 'connected', 'args': []})
 
     while True:
         try:
