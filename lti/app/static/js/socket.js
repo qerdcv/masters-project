11535801aa089fe2472c39443b65c1fa30235660
qerdcv <vadym.tishchenko2@gmail.com>
2023-11-16 01:56:38 +0200

    Update lti template layout

--- lti/app/static/js/socket.js
+++ lti/app/static/js/socket.js
@@ -1,55 +1,65 @@
-const serverState = document.getElementById('server-state');
+const serverState = document.getElementById("server-state");
 const ws = new WebSocket(`wss://lti.lapers.net/ws/client/${email}`);
 
 ws.onmessage = (e) => {
-    handleEvent(JSON.parse(e.data));
+  handleEvent(JSON.parse(e.data));
 };
 
 ws.onclose = (e) => {
-    console.log('websocket close ->\n', e.reason);
+  console.log("websocket close ->\n", e.reason);
 };
 
 function handleEvent(e) {
-    switch (e.event) {
-    case 'connected':
-        serverState.textContent = 'on';
-        serverState.style.color = 'green';
-        break;
-    case 'disconnected':
-        serverState.textContent = 'off';
-        serverState.style.color = 'red';
-        break;
-        case 'test_result':
-        for (let test of e.args) {
-            const testEl = document.getElementById(`test-${test.name}`);
-
-            console.log(test);
-            const detail = document.createElement('span')
-            if (test.result.status === 'success') {
-                detail.textContent = ' Success'
-                detail.style.color = 'green'
-            } else {
-                detail.textContent = ' Error: ' + test.result.error;
-                detail.style.color = 'red';
-
-            }
-
-            testEl.appendChild(detail);
+  switch (e.event) {
+    case "connected":
+      serverState.textContent = "on";
+      serverState.style.color = "green";
+      break;
+    case "disconnected":
+      serverState.textContent = "off";
+      serverState.style.color = "red";
+      break;
+    case "test_result":
+      for (let test of e.args) {
+        const testEl = document.getElementById(`test-${test.name}`);
+        let detail;
+        switch (test.result.status) {
+          case "success":
+            detail = document.createElement("span");
+            detail.classList.add("alert", "alert-success");
+            detail.textContent = "Success";
+            break;
+          case "failed":
+            detail = document.createElement("detail");
+            detail.classList.add("alert", "alert-danger");
+            detail.innerText = `
+                <summary>Error</summary>
+                <pre>${test.result.error}</pre>
+              `;
+            break;
+          default:
+            detail = document.createElement("span");
+            detail.textContent = "Unknown error happened!";
+            detail.classList.add("alert", "alert-danger");
         }
 
-        // todo: move grades to the server side
-        const score = e.args.reduce((acc, test) => {
-            if (test.result.status === 'success') {
-                return acc + 1;
-            }
+        testEl.querySelector(".result").innerHTML = detail.outerHTML;
+      }
 
-            return acc;
-        }, 0);
+      const score = e.args.reduce((acc, test) => {
+        if (test.result.status === "success") {
+          return acc + 1;
+        }
 
-        console.log(score, e.args.length);
-        fetch(`/api/score/${launchID}/${Math.trunc((score / e.args.length) * 100)}`, {
-            method: 'POST'
-        }).catch(console.error)
-        break;
-    }
+        return acc;
+      }, 0);
+
+      fetch(
+        `/api/score/${launchID}/${Math.trunc((score / e.args.length) * 100)}`,
+        {
+          method: "POST",
+        }
+      ).catch(console.error);
+      break;
+  }
 }
